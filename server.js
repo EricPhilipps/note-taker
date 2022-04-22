28ad842579f12165b32a80a70a2ee1d9d5e1847b
Eric <ericphilipps123@gmail.com>
2022-04-21 21:13:02 -0500

    moved routes to server file and worked with gets

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
 app.use(express.static('public'));
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
 
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+
+})
+
+app.post("/api/notes", function(req, res) {
+
+})
