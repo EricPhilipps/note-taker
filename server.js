f0c5208fd0d89502ce211aa3f7985f7028bdb6e4
Eric <ericphilipps123@gmail.com>
2022-04-21 20:21:38 -0500

    further set up and beginning gets/posts

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
 app.use(express.static('public'));
-app.listen(PORT, () => console.log(`Listening on PORT: PORT`));
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
