1c026dfd8da386068ef95e44c761c10c52928e3e
Eric <ericphilipps123@gmail.com>
2022-05-22 17:06:27 -0500

    Notes displaying and posting finally

--- server.js
+++ server.js
@@ -9,26 +9,32 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
 app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
+    let allNotes = JSON.parse(fs.readFileSync("./db/db.json"));
+    res.json(allNotes);
 });
 
 app.post("/api/notes", function(req, res) {
-    let note = req.body;
+    let newNote = req.body;
     let noteList = JSON.parse(fs.readFileSync("./db/db.json"));
 
-    noteList.push(note);
+    noteList.push(newNote);
 
     fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
     res.json(noteList);
 });
+
+app.delete("/api/notes:id", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
